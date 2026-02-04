8a607703c974ed4be56ce73e6ff9af861bafb73e
svemula1-07 <svemula1@student.gitam.edu>
2026-02-04 11:11:19 +0530

    Create string_related_program.py

---
+++ string_related_program.py
@@ -0,0 +1,74 @@
+s1 = "Hello"
+s2 = 'v sri siddarth'
+s3 = """Welcome to python class"""
+
+print("s1 =", s1)
+print("s2 =", s2)
+print("s3 =", s3)
+print()
+
+print("First character of s1:", s1[0])        
+print("Last character of s1:", s1[-1])        
+print()
+
+
+s4 = s1 + " " + s2
+print("Concatenation (s1 + ' ' + s2):", s4)
+
+s5 = s1 * 3
+print("Repetition (s1 * 3):", s5)
+
+
+print("Length of s3:", len(s3))
+
+
+print("'Py' in s2?", "Py" in s2)
+print("'java' not in s2?", "java" not in s2)
+print()
+
+
+text = "Programming"
+print("text =", text)
+
+print("text[0:4]   =", text[0:4])     
+print("text[3:]    =", text[3:])      
+print("text[:5]    =", text[:5])      
+print("text[-4:]   =", text[-4:])     
+print("text[::2]   =", text[::2])     
+print("text[::-1]  =", text[::-1])    
+print()
+
+
+sample = "   hello python world   "
+print("Original sample: '", sample, "'", sep="")
+
+
+print("strip()  -> '", sample.strip(), "'", sep="")
+
+
+print("upper()   ->", sample.upper())
+print("lower()   ->", sample.lower())
+
+print("replace('python', 'Java') ->", sample.replace("python", "Java"))
+
+
+words = sample.strip().split()
+print("split() ->", words)
+
+joined = "-".join(words)
+print("'-'.join(words) ->", joined)
+
+
+print("find('python') ->", sample.find("python"))
+
+
+print("count('l') ->", sample.count("l"))
+
+print("startswith('   he') ->", sample.startswith("   he"))
+print("endswith('world   ') ->", sample.endswith("world   "))
+
+
+only_letters = "HelloWorld"
+only_digits = "12345"
+print("'HelloWorld'.isalpha() ->", only_letters.isalpha())
+print("'12345'.isdigit() ->", only_digits.isdigit())          
