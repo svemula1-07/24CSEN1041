dadc6aa2a94fc693dcabad5340268232083e8e34
svemula1-07 <svemula1@student.gitam.edu>
2026-02-09 14:52:18 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+list_1 = ['2025102233' , "v siddarth", "5", "4", "7", "apple", "2.00"]
+print(list_1)
+print(type(list_1))
+print(list_1[-4])
+print(len(list_1))
+print(len(list_1[4]))
+
+OUTPUT
+['2025095919', 'devendra', '5', '4', '7', 'apple', '2.00']
+<class 'list'>
+4
+7
+1
